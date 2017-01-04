943b0a866064863855235d0bf6762b138195cc7b
Kimusabi <thomas.hull89@gmail.com>
2017-01-04 21:42:52 +1030

    Testing and Notes

--- CampaignRegistry.lua
+++ CampaignRegistry.lua
@@ -1,4 +1,5 @@
 {
 	["OptDDCL"] = "desktopdecal_COS1",
 	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel,background,class,feat,race,skill,spell",
+	["OptMANUALROLL"] = "off",
 }
