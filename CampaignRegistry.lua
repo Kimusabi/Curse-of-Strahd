84bddf26ff6a8b8fb71e146fb2541f204cfe1ea8
Kimusabi <thomas.hull89@gmail.com>
2017-02-09 19:32:25 +1030

    Revert "Fixed issues!"
    This reverts commit d224c3864ab92d39f1a0eca7dcc7134f463ef865.

--- CampaignRegistry.lua
+++
@@ -1,5 +0,0 @@
-{
-	["OptDDCL"] = "desktopdecal_COS1",
-	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel,background,class,feat,race,skill,spell",
-	["OptMANUALROLL"] = "off",
-}
